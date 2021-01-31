63968e1f5721359f2b985fd99b16cda4b4fd20b3
Lukita da Galera <lucasgo1099@gmail.com>
2021-01-30 22:49:35 -0300

    adicionando imagens

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  /* Rodando o projeto*/
+  runApp(MaterialApp(
+      title: "Contador de pessoas",
+      /* widgem na coluna*/
+      home: Stack(
+        children: <Widget>[
+          
+            Image.asset(
+              "imagens/restaurant.jpg",
+              fit: BoxFit.cover,
+              height: 1000.0,
+            ),
+
+          Column(
+
+            /* Alinhando a coluna */
+            mainAxisAlignment: MainAxisAlignment.center,
+            /* Adicionado os widget = componentes */
+            children: <Widget>[
+              Text(
+                "Pessoas: 0",
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+
+
+              Row(
+                /* alinhando no eixo horizontal */
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child:    /* Criando o button*/
+                    FlatButton(
+                      child: Text("+ 1",
+                          style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                      onPressed: () {},
+                    ),
+
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child:    /* Criando o button*/
+                    FlatButton(
+                      child: Text("-1",
+                          style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                      onPressed: () {},
+                    ),
+
+                  ),
+
+                ],
+              ),
+
+
+              Text(
+                "Pode entrar senhor(a)",
+                style: TextStyle(
+                    color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
+              )
+            ],
+          )
+        ],
+      )));
+}
