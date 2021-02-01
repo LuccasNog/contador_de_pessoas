7ebd5da906874c7a05f13c38ee598f979bc635c9
Lukita da Galera <lucasgo1099@gmail.com>
2021-02-01 20:05:47 -0300

    finish project

--- lib/main.dart
+++ lib/main.dart
@@ -5,65 +5,111 @@ void main() {
   runApp(MaterialApp(
       title: "Contador de pessoas",
       /* widgem na coluna*/
-      home: Stack(
-        children: <Widget>[
-          
-            Image.asset(
-              "imagens/restaurant.jpg",
-              fit: BoxFit.cover,
-              height: 1000.0,
+      home: Home()));
+}
+
+
+// Criando Widget STFULL
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // Quantidade de pessoas vamos passar para o texto.
+  int _people = 0;
+  String _infoText = "Pode entrar";
+
+  // Função que vai receber mais 1 qyabdi for pra incrementar
+
+  void _changePeople(int delta){
+    // executar o código e mandar atualizar a tela
+    setState(() {
+      //somar 1 ou subtrair um
+      _people += delta;
+      if(_people < 0){
+         _infoText =  "Liberado";
+      }else if(_people <= 10){
+        _infoText =  "Pode entrar";
+      }
+      else{
+        _infoText =  "Lotado";
+      }
+    });
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+
+        Image.asset(
+          "imagens/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+
+        Column(
+
+          /* Alinhando a coluna */
+          mainAxisAlignment: MainAxisAlignment.center,
+          /* Adicionado os widget = componentes */
+          children: <Widget>[
+            Text(
+              "Pessoas: $_people",
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             ),
 
-          Column(
 
-            /* Alinhando a coluna */
-            mainAxisAlignment: MainAxisAlignment.center,
-            /* Adicionado os widget = componentes */
-            children: <Widget>[
-              Text(
-                "Pessoas: 0",
-                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
+            Row(
+              /* alinhando no eixo horizontal */
+              mainAxisAlignment: MainAxisAlignment.center,
 
-
-              Row(
-                /* alinhando no eixo horizontal */
-                mainAxisAlignment: MainAxisAlignment.center,
-
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child:    /* Criando o button*/
-                    FlatButton(
-                      child: Text("+ 1",
-                          style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                      onPressed: () {},
-                    ),
-
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child:    /* Criando o button*/
+                  FlatButton(
+                    child: Text("+ 1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
                   ),
 
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child:    /* Criando o button*/
-                    FlatButton(
-                      child: Text("-1",
-                          style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                      onPressed: () {},
-                    ),
+                ),
 
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child:    /* Criando o button*/
+                  FlatButton(
+                    child: Text("-1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
                   ),
 
-                ],
-              ),
+                ),
+
+              ],
+            ),
 
 
-              Text(
-                "Pode entrar senhor(a)",
-                style: TextStyle(
-                    color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
-              )
-            ],
-          )
-        ],
-      )));
+            Text(
+              _infoText,
+              style: TextStyle(
+                  color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
+            )
+          ],
+        )
+      ],
+    );
+  }
 }
+
+
+
